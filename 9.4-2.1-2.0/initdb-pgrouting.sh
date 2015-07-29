be3a87112f4628055773bacd6abc80061690e1b3
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-07-29 10:09:51 +0200

    Update initdb-postgis.sh with upstream changes
    Related: appropriate/docker-postgis#9
    Related: docker-library/postgres#75

--- 9.4-2.1-2.0/initdb-pgrouting.sh
+++ 9.4-2.1-2.0/initdb-pgrouting.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-POSTGRES="gosu postgres postgres"
+set -e
 
-$POSTGRES --single postgres -E <<EOSQL
+# Perform all actions as user 'postgres'
+export PGUSER=postgres
+
+# Create the 'template_postgis' template db
+psql <<EOSQL
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
 CREATE EXTENSION pgrouting;
 EOSQL
-
