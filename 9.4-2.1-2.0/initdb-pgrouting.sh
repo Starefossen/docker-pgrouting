e91914471f7ca3534d72755ff5b743be95b03948
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-09-13 21:58:46 +0200

    Run init pgRouting setup on correct database

--- 9.4-2.1-2.0/initdb-pgrouting.sh
+++ 9.4-2.1-2.0/initdb-pgrouting.sh
@@ -4,9 +4,8 @@ set -e
 # Perform all actions as user 'postgres'
 export PGUSER=postgres
 
-# Create the 'template_postgis' template db
-psql <<EOSQL
+# Add pgRouting Functions to the database
+psql --dbname="$POSTGRES_DB" <<EOSQL
 CREATE EXTENSION postgis;
-CREATE EXTENSION postgis_topology;
 CREATE EXTENSION pgrouting;
 EOSQL
